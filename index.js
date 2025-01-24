4ae4794526b2f99d2fd0b9a4c6f2278f791917d2
itzVort3x1 <saikaustubh10@gmail.com>
2025-01-24 22:09:30 +0000

    added cors handling

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import express from "express";
 import { mailRouter } from "./routes/sendAdminEmail.js";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 const app = express();
 const PORT = 4000;
+
+app.use(cors());
 
 app.use(express.json());
 
